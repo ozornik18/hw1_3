d83114461283b2edefb113464bf69fcf15a4c6c3
Константин <kuz18rus@ya.ru>
2022-12-04 18:34:47 +0400

    выполнение задания 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-       // Задание 1
-
+        // Задание 1
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("поздравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("возраст совершеннолетия ещё не наступил, нужно немного подождать.");
+        }
     }
 }
